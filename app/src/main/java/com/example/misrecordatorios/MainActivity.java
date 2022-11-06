b4c54efbe064c2fe57ee6095bd87581046b20401
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-06 14:21:14 +0100

    Metodo updateUI()

--- app/src/main/java/com/example/misrecordatorios/MainActivity.java
+++ app/src/main/java/com/example/misrecordatorios/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.misrecordatorios;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private TaskData
+    private TareaDB tareaDB;
+    private ListView ListaTareas;
+    private ArrayAdapter<String> arrayAdapter;
 
 
 
@@ -16,6 +24,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tareaDB = new TareaDB(this);
+        ListaTareas = (ListView) findViewById(R.id.list_todo);
+
+        updateUI();
+    }
+
+    private void updateUI(){
+        ArrayList<String> listaTareas = new ArrayList<>();
+        SQLiteDatabase bd = tareaDB.getReadableDatabase();
+        Cursor cursor = bd.query(Tarea.TareaEntrada.TABLA, new String[]{Tarea.TareaEntrada._ID, Tarea.TareaEntrada.COL_TAREA_TITULO},
+                null, null, null, null, null);
+        while(cursor.moveToNext()){
+            int idx = cursor.getColumnIndex(Tarea.TareaEntrada.COL_TAREA_TITULO);
+            listaTareas.add(cursor.getString(idx));
+        }
+
+        if (arrayAdapter == null){
+            arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_nueva_tarea, listaTareas);
+            ListaTareas.setAdapter(arrayAdapter);
+        } else {
+            arrayAdapter.clear();
+            arrayAdapter.addAll(listaTareas);
+            arrayAdapter.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        bd.close();
     }
 
 
--- app/src/main/java/com/example/misrecordatorios/TareaDB.java
+++ app/src/main/java/com/example/misrecordatorios/TareaDB.java
@@ -1,4 +1,29 @@
 package com.example.misrecordatorios;
 
-public class TareaDB {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TareaDB extends SQLiteOpenHelper {
+
+    public TareaDB(Context context){
+        super(context, Tarea.BD_NOMBRE, null, Tarea.BD_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase bd) {
+        String crearTabla = "CREATE TABLE " + Tarea.TareaEntrada.TABLA + " ( " +
+                Tarea.TareaEntrada._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Tarea.TareaEntrada.COL_TAREA_TITULO + " TEXT NOT NULL);";
+
+        bd.execSQL(crearTabla);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase bd, int oldVersion, int newVersion) {
+        bd.execSQL("DROP TABLE IF EXISTS " + Tarea.TareaEntrada.TABLA);
+        onCreate(bd);
+
+    }
 }
