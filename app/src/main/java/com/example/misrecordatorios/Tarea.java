6f9122caea0e0610fde2a07b7ddc4c513ebd3f28
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-06 14:02:45 +0100

    Clases Tarea y TareaDB creadas

--- app/src/main/java/com/example/misrecordatorios/Tarea.java
+++ app/src/main/java/com/example/misrecordatorios/Tarea.java
@@ -1,4 +1,13 @@
 package com.example.misrecordatorios;
 
+import android.provider.BaseColumns;
+
 public class Tarea {
+    public static final String BD_NOMBRE = "com.aziflaj.todolist.db";
+    public static final int BD_VERSION = 1;
+
+    public class TareaEntrada implements BaseColumns {
+        public static final String TABLA = "tareas";
+        public static final String COL_TAREA_TITULO = "titulo";
+    }
 }
---
+++ app/src/main/java/com/example/misrecordatorios/TareaDB.java
@@ -0,0 +1,4 @@
+package com.example.misrecordatorios;
+
+public class TareaDB {
+}
